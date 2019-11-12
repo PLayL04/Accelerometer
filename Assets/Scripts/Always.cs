d5392f80ea57f732c2cf1d2cf6563070b51a73f0
Антон Черногор <chernogor.24@mail.ru>
2019-11-12 18:48:36 +0700

    отчет

---
+++ Assets/Scripts/Always.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Always : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Settings.valueVol = PlayerPrefs.GetFloat("Volume");
+        Settings.valueSens = PlayerPrefs.GetFloat("Sensevity");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    public void Slider()
+    public static float valueSens;
+    public static float valueVol;
+    public Slider sliderVol;
+    public Slider sliderSens;
+
+    private void Update()
+    {
+        valueVol = sliderVol.value;
+        valueSens = sliderSens.value;
+        PlayerPrefs.SetFloat("Volume", valueVol);
+        PlayerPrefs.SetFloat("Sensevity", valueSens);
+    }
+    public void Volume() // Звук
     {
         // заглушка
+    }
+
+    public void Sensevity() // Чувствительность
+    {
+
     }
 }
